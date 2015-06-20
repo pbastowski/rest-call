039853e5489cc0d554b68159c9ca0749dbc12991
Paul Bastowski <pbastowski@gmail.com>
2015-06-20 21:40:07 +0200

    Updated dependency to latest angular2-now 0.2.9

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name:          'pbastowski:rest-call',
-    version:       '0.0.3',
+    version:       '0.0.4',
     summary:       'RestCall annotation for angular2-now',
     git:           'https://github.com/pbastowski/rest-call',
     documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
     api.use('pbastowski:angular-babel@0.1.5');
-    api.use('pbastowski:angular2-now@0.2.8');
+    api.use('pbastowski:angular2-now@0.2.9');
     api.addFiles('rest-call.es6.js', ['client']);
 });
 
