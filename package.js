e83503ff91c2e5ffa352ad9514a18dad5f81e10b
Paul Bastowski <pbastowski@gmail.com>
2015-06-20 20:07:20 +0200

    First working version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
-  name: 'pbastowski:rest-service',
-  version: '0.0.2',
-  summary: 'RestCall annotations for angular2-now',
+  name: 'pbastowski:rest-call',
+  version: '0.0.3',
+  summary: 'RestCall annotation for angular2-now',
   git: '',
   documentation: 'README.md'
 });
@@ -10,11 +10,11 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use('pbastowski:angular-babel@0.1.5');
   api.use('pbastowski:angular2-now@0.2.8');
-  api.addFiles('rest-service.es6.js', ['client']);
+  api.addFiles('rest-call.es6.js', ['client']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('rest-service.es6.js');
-  api.addFiles('rest-service-tests.js');
+  api.use('rest-call.es6.js');
+  api.addFiles('rest-call-tests.js');
 });
