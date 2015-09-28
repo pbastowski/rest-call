5149e87662afc38d37e7f01b4da6c343d3b38d84
Paul Bastowski <pbastowski@gmail.com>
2015-09-28 13:40:26 +0200

    Removed dependency on pbastowski:angular-babel

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name:          'pbastowski:rest-call',
-    version:       '0.0.9',
+    version:       '0.0.10',
     summary:       'RestCall annotation for angular2-now',
     git:           'https://github.com/pbastowski/rest-call',
     documentation: 'README.md'
@@ -8,13 +8,6 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
-    api.use('pbastowski:angular-babel@0.1.5');
     api.use('pbastowski:angular2-now@0.2.9');
     api.addFiles('rest-call.es6.js', ['client']);
 });
-
-//Package.onTest(function(api) {
-//  api.use('tinytest');
-//  api.use('rest-call.es6.js');
-//  api.addFiles('rest-call-tests.js');
-//});
