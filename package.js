853b7c114fa5737b945ee369e748e18f0bba7fa6
Paul Bastowski <pbastowski@gmail.com>
2015-11-15 13:17:39 +0100

    TypeScript version for Meteor 1.2+

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
-    name:          'pbastowski:rest-call',
-    version:       '0.1.1',
+    name:          'xpbastowski:rest-call',
+    version:       '1.0.0',
     summary:       'RestCall annotation for angular2-now',
     git:           'https://github.com/pbastowski/rest-call',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('1.1.0.2');
-    api.use('pbastowski:angular2-now@0.2.9');
-    api.addFiles('rest-call.es6.js', ['client']);
+    api.versionsFrom('1.2.0');
+    //api.use('pbastowski:angular2-now@0.2.9');
+    //api.use('pbastowski:ng2now');
+    api.use('pbastowski:typescript');
+    api.addFiles('rest-call.ts', ['client']);
 });
