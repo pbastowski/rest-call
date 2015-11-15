81212c16d27cf2133578fc48f38176bc6827a479
Paul Bastowski <pbastowski@gmail.com>
2015-11-15 13:23:02 +0100

    Fixed incorrect Meteor version in api.use

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name:          'xpbastowski:rest-call',
+    name:          'pbastowski:rest-call',
     version:       '1.0.0',
     summary:       'RestCall annotation for angular2-now',
     git:           'https://github.com/pbastowski/rest-call',
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('1.2.0');
+    api.versionsFrom('1.2.0.1');
     //api.use('pbastowski:angular2-now@0.2.9');
     //api.use('pbastowski:ng2now');
     api.use('pbastowski:typescript');
