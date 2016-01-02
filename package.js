1ca5ceacc670f9be85034383125b0c0b14eb7362
Paul Bastowski <pbastowski@gmail.com>
2016-01-02 17:55:33 +0100

    Mateor 1.2+ version of rest-call

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name:          'pbastowski:rest-call',
-    version:       '0.1.1',
+    version:       '0.2.0',
     summary:       'RestCall annotation for angular2-now',
     git:           'https://github.com/pbastowski/rest-call',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('1.1.0.2');
-    api.use('pbastowski:angular2-now@0.2.9');
+    api.versionsFrom('1.2.0.1');
+    api.use('pbastowski:angular2-now@1.0.1');
     api.addFiles('rest-call.es6.js', ['client']);
 });
